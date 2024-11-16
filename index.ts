99dfe1303c99fa3fa6b3848c535cbf15707dcc0e
Rio <rio@r26.me>
2024-11-16 00:43:39 -0500

    hook up speaker mode and add basic styling

--- index.ts
+++ index.ts
@@ -4,12 +4,13 @@ function init() {
    const buttonElement = document.getElementById('generate-button');
    const fileElement = document.getElementById('midi-upload');
    const outputElement = document.getElementById('bp-output');
+   const modeElement = document.getElementById('speaker-mode');
 
-   if(fileElement instanceof HTMLInputElement) {
+   if(fileElement instanceof HTMLInputElement && modeElement instanceof HTMLSelectElement) {
       buttonElement.onclick = async () => {
          if(fileElement.files.length > 0) {
             const file = fileElement.files[0];
-            const bp = await midiToBP(await file.arrayBuffer(), 'surface');
+            const bp = await midiToBP(await file.arrayBuffer(), modeElement.value);
             outputElement.textContent = bp;
          }
       };
